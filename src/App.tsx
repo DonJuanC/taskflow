b276f128b9a8bca05cd07aeede1029f4acbab3a0
Juan Camilo Castellanos Uribe <juancamilo891018@gmail.com>
2026-07-28 06:54:06 -0500

    Hito 4: rutas protegidas con ProtectedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Register } from "./pages/Register/Register";
 import { Login } from "./pages/Login/Login";
+import { ProtectedRoute } from "./routes/ProtectedRoute";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<div>Home (temporal)</div>} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <div>Home (temporal)</div>
+            </ProtectedRoute>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
